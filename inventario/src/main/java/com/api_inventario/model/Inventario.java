326009281382d1ce9cefa6f01ce370ae228b50f7
benjatussy <benja.contrerasg@duocuc.cl>
2025-06-04 18:11:44 -0400

    Se creo repository

--- inventario/src/main/java/com/api_inventario/model/Inventario.java
+++ inventario/src/main/java/com/api_inventario/model/Inventario.java
@@ -1,5 +1,26 @@
 package com.api_inventario.model;
 
+import lombok.*;
+import jakarta.persistence.*;
+
+@Data
+@Entity
+@Table(name = "inventario")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Inventario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="id_inventario")
+    private Integer id;
+
+    @Column(name="id_producto")
+    private Integer idProducto;
+
+    @Column(name="stock_disponible")
+    private Integer stockDisponible;
+
+    private String ubicacion_bodega;
 
 }
---
+++ inventario/src/main/java/com/api_inventario/repository/InventarioRepository.java
@@ -0,0 +1,12 @@
+package com.api_inventario.repository;
+
+import com.api_inventario.model.Inventario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventarioRepository extends JpaRepository<Inventario, Integer> {
+    
+   
+
+}
