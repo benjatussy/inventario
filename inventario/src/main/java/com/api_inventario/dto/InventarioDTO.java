173742363bc3b0857cef2f8c967c7d1575bb257e
benjatussy <benja.contrerasg@duocuc.cl>
2025-06-04 18:24:20 -0400

    DTO invetario creado

---
+++ inventario/src/main/java/com/api_inventario/dto/InventarioDTO.java
@@ -0,0 +1,16 @@
+package com.api_inventario.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InventarioDTO {
+    private Integer id;
+    private Integer idProducto;
+    private Integer stockDisponible;
+    private String ubicacionBodega;
+
+
+
+}
