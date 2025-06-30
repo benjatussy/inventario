0bda1892387b545af692c735a1258dd8aab784d4
benjatussy <benja.contrerasg@duocuc.cl>
2025-06-30 19:06:31 -0400

    agregado mas cosas de hateoas

--- inventario/src/main/java/com/api_inventario/controller/InventarioController.java
+++ inventario/src/main/java/com/api_inventario/controller/InventarioController.java
@@ -9,10 +9,9 @@ import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo; 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
+import org.springframework.hateoas.Link;
 
 import com.api_inventario.dto.InventarioDTO;
-import com.api_inventario.model.Inventario;
 import com.api_inventario.service.InventarioService;
 
 @RestController
@@ -48,27 +47,38 @@ public class InventarioController {
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/hateoas/{id}")
+       @GetMapping("/hateoas/{id}")
     public InventarioDTO obtenerHATEOAS(@PathVariable Integer id) {
         InventarioDTO dto = inventarioService.obtenerPorId(id);
-        
+        if (dto == null) {
+            return null;
+        }
+
         dto.add(linkTo(methodOn(InventarioController.class).obtenerHATEOAS(id)).withSelfRel());
         dto.add(linkTo(methodOn(InventarioController.class).obtenerTodosHATEOAS()).withRel("todos"));
         dto.add(linkTo(methodOn(InventarioController.class).eliminar(id)).withRel("eliminar"));
 
+
+        dto.add(Link.of("http://localhost:8088/api/proxy/inventario/" + dto.getId()).withSelfRel());
+        dto.add(Link.of("http://localhost:8088/api/proxy/inventario/" + dto.getId()).withRel("Modificar HATEOAS").withType("PUT"));
+        dto.add(Link.of("http://localhost:8088/api/proxy/inventario/" + dto.getId()).withRel("Eliminar HATEOAS").withType("DELETE"));
+
         return dto;
     }
 
-    //METODO HATEOAS para listar todos los productos utilizando HATEOAS
+    
     @GetMapping("/hateoas")
     public List<InventarioDTO> obtenerTodosHATEOAS() {
         List<InventarioDTO> lista = inventarioService.listar();
 
         for (InventarioDTO dto : lista) {
+       
             dto.add(linkTo(methodOn(InventarioController.class).obtenerHATEOAS(dto.getId())).withSelfRel());
+
+            dto.add(Link.of("http://localhost:8088/api/proxy/inventario").withRel("Get todos HATEOAS"));
+            dto.add(Link.of("http://localhost:8088/api/proxy/inventario/" + dto.getId()).withRel("Crear HATEOAS").withType("POST"));
         }
 
         return lista;
     }
-
 }
